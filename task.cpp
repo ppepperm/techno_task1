e4be1910db1ff2487df2ae68d7aeefa778b2ab88
pepperm <arseniy@medinskiy.net>
2020-09-24 21:33:00 +0300

    aaa

--- task.cpp
+++ task.cpp
@@ -21,9 +21,11 @@ int	find_nth_occ(string str, int n)
 {
 	int iter = 0;
 
+	//cout << n << " dfsadasdfsa"<<endl;
 	for(int k = 0; k < n; k++)
 	{
 		iter = str.find_first_of(" ", iter);
+		//cout << iter << " jopa"<< endl;
 		if (iter == -1)
 		{
 			return -1;
@@ -56,20 +58,26 @@ int	main(int ac, char **av)
 		string num;
 		string op;
 		int index;
+		int	N;
 
 		iter = string(av[i]);
 		index = iter.find_first_of(":");
 		num = iter.substr(0,index);
 		op = iter.substr(index + 1);
+		N = atoi(num.c_str());
 		if (op == "u")
 		{
 			int begin, end;
 			string field;
 
-			cout << num << " to lower" << endl;
+			cout << N << " to lower" << endl;
 			for (int i = 0; i < strings.size(); i ++)
 			{
-
+				begin = find_nth_occ(strings[i], N);
+				end = find_nth_occ(strings[i], N + 1);
+				//cout << begin << " "<< end<<endl;
+				field = strings[i].substr(begin, end);
+				cout << field << endl;
 			}
 
 		}
@@ -78,12 +86,31 @@ int	main(int ac, char **av)
 			int begin, end;
 			string field;
 
-			cout << num << " to upper" << endl;
+			cout << N << " to upper" << endl;
+			for (int i = 0; i < strings.size(); i ++)
+			{
+				begin = find_nth_occ(strings[i], N);
+				end = find_nth_occ(strings[i], N + 1);
+				//cout << begin << " "<< end<<endl;
+				field = strings[i].substr(begin, end);
+				cout << field << endl;
 
+			}
 		}
 		else if (op.length() == 2)
 		{
-			cout << num << " swap " << op[0] << " to " << op[1]<< endl;
+			int begin, end;
+			string field;
+
+			cout << N << " swap " << op[0] << " to " << op[1]<< endl;
+			for (int i = 0; i < strings.size(); i ++)
+			{
+				begin = find_nth_occ(strings[i], N);
+				end = find_nth_occ(strings[i], N + 1);
+				//cout << begin << " "<< end<<endl;
+				field = strings[i].substr(begin, end);
+				cout << field << endl;
+			}
 		}
 	}
 }
