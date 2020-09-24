182862d5a256379fa999d8bdced070bb0b0c0385
pepperm <arseniy@medinskiy.net>
2020-09-24 22:00:07 +0300

    aaa

--- task.cpp
+++ task.cpp
@@ -24,15 +24,23 @@ int	find_nth_occ(string str, int n)
 	//cout << n << " dfsadasdfsa"<<endl;
 	for(int k = 0; k < n; k++)
 	{
-		iter = str.find_first_of(" ", iter);
+		iter = str.find_first_of(" ", iter + 1);
 		//cout << iter << " jopa"<< endl;
 		if (iter == -1)
 		{
 			return -1;
 		}
-		k++;
 	}
 	return iter;
+}
+
+void swap_char(string &string, char from, char to)
+{
+	for (int i = 0; i < string.size(); i++)
+	{
+		if (string[i] == from)
+			string[i] = to;
+	}
 }
 
 int	main(int ac, char **av)
@@ -48,10 +56,7 @@ int	main(int ac, char **av)
 		strings.push_back(iter);
 	}
 
-	for (int i = 0; i < strings.size(); i ++)
-	{
-		cout << strings[i]<< endl;
-	}
+	int *ids = new int [strings.size()];
 
 	for (int i = 2; i < ac; i++)
 	{
@@ -70,31 +75,37 @@ int	main(int ac, char **av)
 			int begin, end;
 			string field;
 
-			cout << N << " to lower" << endl;
 			for (int i = 0; i < strings.size(); i ++)
 			{
 				begin = find_nth_occ(strings[i], N);
 				end = find_nth_occ(strings[i], N + 1);
-				//cout << begin << " "<< end<<endl;
-				field = strings[i].substr(begin, end);
-				cout << field << endl;
+				if (begin < 0)
+					continue ;
+				ids[i] = 1;
+				if (begin > 0)
+					begin ++;
+				field = strings[i].substr(begin, end - begin);
+				transform(field.begin(), field.end(), field.begin(), ::tolower);
+				strings[i].replace(begin, end - begin, field);
 			}
-
 		}
 		else if (op == "U")
 		{
 			int begin, end;
 			string field;
 
-			cout << N << " to upper" << endl;
 			for (int i = 0; i < strings.size(); i ++)
 			{
 				begin = find_nth_occ(strings[i], N);
 				end = find_nth_occ(strings[i], N + 1);
-				//cout << begin << " "<< end<<endl;
-				field = strings[i].substr(begin, end);
-				cout << field << endl;
-
+				if (begin < 0)
+					continue ;
+				ids[i] = 1;
+				if (begin > 0)
+					begin ++;
+				field = strings[i].substr(begin, end - begin);
+				transform(field.begin(), field.end(), field.begin(), ::toupper);
+				strings[i].replace(begin, end - begin, field);
 			}
 		}
 		else if (op.length() == 2)
@@ -102,15 +113,24 @@ int	main(int ac, char **av)
 			int begin, end;
 			string field;
 
-			cout << N << " swap " << op[0] << " to " << op[1]<< endl;
 			for (int i = 0; i < strings.size(); i ++)
 			{
 				begin = find_nth_occ(strings[i], N);
 				end = find_nth_occ(strings[i], N + 1);
-				//cout << begin << " "<< end<<endl;
-				field = strings[i].substr(begin, end);
-				cout << field << endl;
+				if (begin < 0)
+					continue ;
+				ids[i] = 1;
+				if (begin > 0)
+					begin ++;
+				field = strings[i].substr(begin, end - begin);
+				swap_char(field, op[0], op[1]);
+				strings[i].replace(begin, end - begin, field);
 			}
 		}
+	}
+	for (int i = 0; i < strings.size(); i++)
+	{
+		if (ids[i] > 0)
+			cout << strings[i]<< endl;
 	}
 }
